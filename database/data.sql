d4b9b005571f15ea113ef9480a4f7a5f46fa71f2
Clinton Hipple <clintondhipple@hotmail.com>
2021-01-05 09:45:25 -0600

    added sql to appropriate db files for docker

---
+++ database/data.sql
@@ -0,0 +1,25 @@
+INSERT INTO "race" (race_label) VALUES 
+  ('WHITE'),
+  ('Hispanic or Latino'),
+  ('Black or African American'),
+  ('Native American or American Indian'),
+  ('Asian or Pacific Islander');
+  
+INSERT INTO "sex" (sex_label) VALUES 
+  ('Male'),
+  ('Female'),
+  ('Other');
+  
+INSERT INTO "education_level" (education_label) VALUES 
+  ('No schooling completed'),
+  ('Nursery school to 8th grade'),
+  ('Some high school, no diploma'),
+  ('High school graduate, diploma or the equivalent (for example: GED)'),
+  ('Some college credit, no degree'),
+  ('Trade/technical/vocational training'),
+  ('Associate degree'),
+  ('Bachelor’s degree'),
+  ('Master’s degree'),
+  ('Professional degree'),
+  ('Doctorate degree');
+  
