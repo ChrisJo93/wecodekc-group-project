1580bc8033e53224cb40deb1f124d747f4015586
Sarah Peters <sarahnpeters@gmail.com>
2021-01-05 16:12:07 -0600

    adding breakpoints

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -13,6 +13,7 @@ import {
   MenuItem,
   Button,
   Chip,
+  Grid,
 } from '@material-ui/core';
 
 class RegisterForm extends Component {
@@ -70,70 +71,144 @@ class RegisterForm extends Component {
       <div>
         <form className="formPanel" onSubmit={this.registerUser}>
           <Typography variant="h3" component="h2" gutterBottom>
-            Register User
+            Mentor Registration
           </Typography>
           {this.props.store.errors.registrationMessage && (
             <h3 className="alert" role="alert">
               {this.props.store.errors.registrationMessage}
             </h3>
           )}
-          <TextField
-            size="small"
-            placeholder="first name"
-            type="text"
-            name="first_name"
-            value={this.state.first_name}
-            required
-            variant="outlined"
-            onChange={this.handleInputChangeFor('first_name')}
-          />
-          <TextField
-            size="small"
-            placeholder="middle name/initial"
-            type="text"
-            name="middle_name"
-            value={this.state.first_name}
-            required
-            variant="outlined"
-            onChange={this.handleInputChangeFor('middle_name')}
-          />
-          <TextField
-            size="small"
-            placeholder="last name"
-            type="text"
-            name="last_name"
-            value={this.state.first_name}
-            required
-            variant="outlined"
-            onChange={this.handleInputChangeFor('last_name')}
-          />
-          <TextField
-            id="birth_date"
-            label="birth_date"
-            type="date"
-            defaultValue="2000-01-01"
-            size="small"
-            variant="outlined"
-          />
-          <FormControl variant="outlined" size="small" fullWidth>
-            <InputLabel id="sex">Sex</InputLabel>
-            <Select
-              labelId="sex"
-              id="sex"
-              value={this.state.sex}
-              onChange={this.handleInputChangeFor('sex')}
-              label="sex"
-            >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              <MenuItem value={'Female'}>Female</MenuItem>
-              <MenuItem value={'Male'}>Male</MenuItem>
+          <Typography variant="h6" component="h3" gutterBottom>
+            Personal Details
+          </Typography>
+          <Grid container spacing={3}>
+            <Grid item xs={12} lg={6}>
+              <TextField
+                size="small"
+                placeholder="first name"
+                type="text"
+                name="first_name"
+                value={this.state.first_name}
+                required
+                variant="outlined"
+                onChange={this.handleInputChangeFor('first_name')}
+              />
+              <TextField
+                size="small"
+                placeholder="middle name/initial"
+                type="text"
+                name="middle_name"
+                value={this.state.first_name}
+                required
+                variant="outlined"
+                onChange={this.handleInputChangeFor('middle_name')}
+              />
+              <TextField
+                size="small"
+                placeholder="last name"
+                type="text"
+                name="last_name"
+                value={this.state.first_name}
+                required
+                variant="outlined"
+                onChange={this.handleInputChangeFor('last_name')}
+              />
+              <TextField
+                id="birth_date"
+                label="birth_date"
+                type="date"
+                defaultValue="2000-01-01"
+                size="small"
+                variant="outlined"
+              />
+              <FormControl variant="outlined" size="small" fullWidth>
+                <InputLabel id="sex">Sex</InputLabel>
+                <Select
+                  labelId="sex"
+                  id="sex"
+                  value={this.state.sex}
+                  onChange={this.handleInputChangeFor('sex')}
+                  label="sex"
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={'Female'}>Female</MenuItem>
+                  <MenuItem value={'Male'}>Male</MenuItem>
 
-              <MenuItem value={'Other'}>Other</MenuItem>
-            </Select>
-          </FormControl>
-          {/* <FormControl>
+                  <MenuItem value={'Other'}>Other</MenuItem>
+                </Select>
+              </FormControl>
+              <TextField
+                placeholder="create username"
+                type="text"
+                name="username"
+                variant="outlined"
+                size="small"
+                value={this.state.username}
+                required
+                onChange={this.handleInputChangeFor('username')}
+              />
+            </Grid>
+            <Grid item xs={12} lg={6}>
+              <TextField
+                size="small"
+                placeholder="email"
+                type="email"
+                name="email"
+                value={this.state.email}
+                variant="outlined"
+                onChange={this.handleInputChangeFor('email')}
+              />
+              <TextField
+                size="small"
+                placeholder="phone number"
+                // type="number"
+                name="phone_number"
+                value={this.state.phone_number}
+                variant="outlined"
+                onChange={this.handleInputChangeFor('phone_number')}
+              />
+              <TextField
+                size="small"
+                placeholder="zipcode"
+                type="number"
+                name="zip_code"
+                value={this.state.zip_code}
+                variant="outlined"
+                onChange={this.handleInputChangeFor('zip_code')}
+              />
+              <TextField
+                size="small"
+                placeholder="company"
+                type="text"
+                name="company"
+                value={this.state.company}
+                variant="outlined"
+                onChange={this.handleInputChangeFor('company')}
+              />
+              <TextField
+                size="small"
+                placeholder="job title"
+                type="text"
+                name="job_title"
+                value={this.state.job_title}
+                variant="outlined"
+                onChange={this.handleInputChangeFor('job_title')}
+              />
+
+              <TextField
+                placeholder="create password"
+                type="password"
+                name="password"
+                variant="outlined"
+                size="small"
+                value={this.state.password}
+                required
+                onChange={this.handleInputChangeFor('password')}
+              />
+
+              {/* <FormControl>
             <InputLabel id="education_level">Chip</InputLabel>
             <Select
               labelId="education_level"
@@ -154,76 +229,8 @@ class RegisterForm extends Component {
               {level}
             </Select>
           </FormControl> */}
-          <TextField
-            size="small"
-            placeholder="email"
-            type="email"
-            name="email"
-            value={this.state.email}
-            variant="outlined"
-            onChange={this.handleInputChangeFor('email')}
-          />
-          <TextField
-            size="small"
-            placeholder="phone number"
-            // type="number"
-            name="phone_number"
-            value={this.state.phone_number}
-            variant="outlined"
-            onChange={this.handleInputChangeFor('phone_number')}
-          />
-          <TextField
-            size="small"
-            placeholder="zipcode"
-            type="number"
-            name="zip_code"
-            value={this.state.zip_code}
-            variant="outlined"
-            onChange={this.handleInputChangeFor('zip_code')}
-          />
-          <TextField
-            size="small"
-            placeholder="company"
-            type="text"
-            name="company"
-            value={this.state.company}
-            variant="outlined"
-            onChange={this.handleInputChangeFor('company')}
-          />
-          <TextField
-            size="small"
-            placeholder="job title"
-            type="text"
-            name="job_title"
-            value={this.state.job_title}
-            variant="outlined"
-            onChange={this.handleInputChangeFor('job_title')}
-          />
-
-          <div>
-            <TextField
-              placeholder="create username"
-              type="text"
-              name="username"
-              variant="outlined"
-              size="small"
-              value={this.state.username}
-              required
-              onChange={this.handleInputChangeFor('username')}
-            />
-          </div>
-          <div>
-            <TextField
-              placeholder="create password"
-              type="password"
-              name="password"
-              variant="outlined"
-              size="small"
-              value={this.state.password}
-              required
-              onChange={this.handleInputChangeFor('password')}
-            />
-          </div>
+            </Grid>
+          </Grid>
           <div>
             <Button
               variant="outlined"
