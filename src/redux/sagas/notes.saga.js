1ee237a206deeb05fa9d823ebde237fd428ffca6
Alex Campbell <alexbe.campbell@gmail.com>
2021-01-05 14:39:58 -0600

    added event reducer

--- src/redux/sagas/notes.saga.js
+++ src/redux/sagas/notes.saga.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { put, take, takeLatest } from 'redux-saga/effects';
+import { put, takeLatest } from 'redux-saga/effects';
 
 function* getNotes(action) {
   try {
