0c0dee7664eb07e6cd8b2d182192de825e5d9897
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-01-05 14:51:08 -0600

    dropdown reducers created as one combined reducer

---
+++ src/redux/reducers/dropdown.reducer.js
@@ -0,0 +1,40 @@
+import { combineReducers } from 'redux';
+
+const educationReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_EDUCATION':
+      return action.payload;
+    case 'UNSET_EDUCATION':
+      return [];
+    default:
+      return state;
+  }
+};
+
+const raceReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_RACE':
+      return action.payload;
+    case 'UNSET_RACE':
+      return [];
+    default:
+      return state;
+  }
+};
+
+const sexReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_SEX':
+      return action.payload;
+    case 'UNSET_SEX':
+      return [];
+    default:
+      return state;
+  }
+};
+
+export default combineReducers({
+  educationReducer,
+  sexReducer,
+  raceReducer,
+});
