cf9d5edcfa8b3208923496ed6d07e5eb07d74f2d
Alex Campbell <alexbe.campbell@gmail.com>
2021-01-06 16:10:17 -0600

    added dispatch and reducer map to event page

--- src/Pages/EventsPage/EventsPage.js
+++ src/Pages/EventsPage/EventsPage.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
+import { Grid } from '@material-ui/core';
 
-class LandingPage extends Component {
-  state = {
-    heading: 'Class Component',
-  };
+class EventsPage extends Component {
+  componentDidMount() {
+    this.props.dispatch({
+      type: 'GET_EVENTS',
+      payload: this.props.store.events,
+    });
+  }
 
   render() {
-    return <div></div>;
+    // const eventsArray = this.props.store.eventReducer.map((item, index) => {
+    //   return (
+    //     <Grid item xs={3} key={index}>
+    //       <EventItem events={item} index={index} {...this.props} />
+    //     </Grid>
+    //   );
+    // });
+
+    return (
+      <div>
+        <div style={{ padding: 20 }}>
+          <Grid
+            container
+            spacing={3}
+            direction="row"
+            justify="space-evenly"
+            alignItems="stretch"
+          >
+            <h2>Click on an event to see its details!</h2>
+          </Grid>
+        </div>
+        <div style={{ padding: 20 }}>
+          <Grid
+            container
+            spacing={3}
+            direction="row"
+            justify="center"
+            alignItems="stretch"
+          >
+            {/* {eventsArray} */}
+          </Grid>
+        </div>
+      </div>
+    );
   }
 }
 
-export default connect(mapStoreToProps)(LandingPage);
+export default connect(mapStoreToProps)(EventsPage);
