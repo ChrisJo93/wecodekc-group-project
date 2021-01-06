ada1b5fd3933cec273d541a18610b1270fc6cd80
Clinton Hipple <clintondhipple@hotmail.com>
2021-01-06 10:42:19 -0600

    fixed user router | null, and race to 1 to 1

--- server/routes/user.router.ts
+++ server/routes/user.router.ts
@@ -17,26 +17,25 @@ router.post(
     const userId: number = parseInt(req.body.id);
     const username: string = <string>req.body.username;
     const password: string = encryptPassword(req.body.user_password);
-    const firstName: string | null = <string>req.body.first_name;
-    const middleName: string | null = <string>req.body.middle_name;
-    const lastName: string | null = <string>req.body.last_name;
-    const company: string | null = <string>req.body.company;
-    const jobTitle: string | null = <string>req.body.job_title;
-    const motivationBio: string | null = <string>req.body.motivation_bio;
-    const experienceBio: string | null = <string>req.body.experience_bio;
-    const customSkills: string | null = <string>req.body.custom_entry_skills;
-    const skills: Array<number> | null = req.body.skills;
-    const timeSlot: Array<number> | null = req.body.time_slot;
-    const educationLevel: Array<number> | null = req.body.education_level;
-    const race: Array<number> | null = req.body.race;
-    const backgroundCheck: boolean | null =
-      req.body.background_check_permission;
-    const sex: number | null = parseInt(req.body.sex);
-    const zipCode: number | null = parseInt(req.body.zip_code);
+    const firstName: string = <string>req.body.first_name;
+    const middleName: string = <string>req.body.middle_name;
+    const lastName: string = <string>req.body.last_name;
+    const company: string = <string>req.body.company;
+    const jobTitle: string = <string>req.body.job_title;
+    const motivationBio: string = <string>req.body.motivation_bio;
+    const experienceBio: string = <string>req.body.experience_bio;
+    const customSkills: string = <string>req.body.custom_entry_skills;
+    const skills: Array<number> = req.body.skills;
+    const timeSlot: Array<number> = req.body.time_slot;
+    const educationLevel: Array<number> = req.body.education_level;
+    const race: number = req.body.race;
+    const backgroundCheck: boolean = req.body.background_check_permission;
+    const sex: number = parseInt(req.body.sex);
+    const zipCode: number = parseInt(req.body.zip_code);
 
     const queryOne: string = `INSERT INTO "user" (username, user_password, first_name, middle_name,
       last_name, company, job_title, motivation_bio, experience_bio, custom_entry_skills,
-      background_check_permission, sex, zip_code, access_level) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $13, ${0}) RETURNING id`;
+      background_check_permission, sex, zip_code, access_level) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $13, ${0}, #14) RETURNING id`;
     pool
       .query(queryOne, [
         username,
@@ -52,36 +51,29 @@ router.post(
         backgroundCheck,
         sex,
         zipCode,
+        race,
       ])
       .then(() => {
-        let array: Array<number> | null = skills;
+        let array: Array<number> = skills;
         for (let index = 0; index < array.length; index++) {
-          let element: number | null = array[index];
+          let element: number = array[index];
           let query: string = `INSERT INTO "user_skills" (user_id, element) VALUES ($1, $2)`;
           pool.query(query, [userId, element]);
         }
       })
       .then(() => {
-        let array: Array<number> | null = timeSlot;
+        let array: Array<number> = timeSlot;
         for (let index = 0; index < array.length; index++) {
-          let element: number | null = array[index];
+          let element: number = array[index];
           let query: string = `INSERT INTO "user_time_slot" (user_id, element) VALUES ($1, $2)`;
           pool.query(query, [userId, element]);
         }
       })
       .then(() => {
-        let array: Array<number> | null = educationLevel;
+        let array: Array<number> = educationLevel;
         for (let index = 0; index < array.length; index++) {
-          let element: number | null = array[index];
+          let element: number = array[index];
           let query: string = `INSERT INTO "user_education_level" (user_id, element) VALUES ($1, $2)`;
-          pool.query(query, [userId, element]);
-        }
-      })
-      .then(() => {
-        let array: Array<number> | null = race;
-        for (let index = 0; index < array.length; index++) {
-          let element: number | null = array[index];
-          let query: string = `INSERT INTO "user_race" (user_id, element) VALUES ($1, $2)`;
           pool.query(query, [userId, element]);
         }
       })
