069f306ef713d8cbd941cc581fc2d5c7200c0480
Alex Campbell <alexbe.campbell@gmail.com>
2021-01-07 10:19:57 -0600

    added events route to app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -19,6 +19,7 @@ import InfoPage from '../../Pages/InfoPage/InfoPage';
 import LandingPage from '../../Pages/LandingPage/LandingPage';
 import LoginPage from '../../Pages/LoginPage/LoginPage';
 import RegisterPage from '../../Pages/RegisterPage/RegisterPage';
+import EventsPage from '../../Pages/EventsPage/EventsPage';
 
 import './App.css';
 
@@ -42,6 +43,13 @@ class App extends Component {
               exact
               path="/about"
               component={AboutPage}
+            />
+
+            <Route
+              // shows EventsPage at all times (logged in or not)
+              exact
+              path="/events"
+              component={EventsPage}
             />
 
             {/* For protected routes, the view could show one of several things on the same route.
