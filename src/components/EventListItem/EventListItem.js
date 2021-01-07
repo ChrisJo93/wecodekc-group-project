a3fbd0f64f8daaebb74254c62d402e6c5f7d476f
Alex Campbell <alexbe.campbell@gmail.com>
2021-01-07 14:20:29 -0600

    EventItem basic skeleton stubbed in, ready for data from database

---
+++ src/components/EventListItem/EventListItem.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import {
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  Button,
+  Typography,
+} from '@material-ui/core';
+
+class EventListItem extends Component {
+  handleCLickDetails = (event) => {
+    // this.props.history.push(`/details/${this.props.eventId}`);
+  };
+
+  render() {
+    const { events } = this.props;
+
+    // const eventsArray = this.props.store.eventReducer.map((item, index) => {
+    //   return (
+    //     <Grid item xs={3} key={index}>
+    //       <EventItem events={item} index={index} {...this.props} />
+    //     </Grid>
+    //   );
+    // });
+    return (
+      <div>
+        <Card>
+          <CardActionArea
+            // key={event.id}
+            onClick={this.handleCLickDetails}
+          >
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {/* {event.name} */}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="ul">
+                {/* <li className="cardList">{event.}</li>
+                <li className="cardList">
+                  {event.}, {event.}
+                </li> */}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button
+              size="small"
+              color="primary"
+              onClick={this.handleCLickDetails}
+            >
+              Details
+            </Button>
+          </CardActions>
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default connect()(withRouter(EventListItem));
