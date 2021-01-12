d28121697a092df0435b3126223d8d4c43642c46
Sarah Peters <sarahnpeters@gmail.com>
2021-01-12 13:58:54 -0600

    changing dialog to form dialog. adding dateclick dialog file

--- src/components/AdminComponents/Calendar/CreateEventDialog.js
+++ src/components/AdminComponents/Calendar/CreateEventDialog.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Avatar from '@material-ui/core/Avatar';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemText from '@material-ui/core/ListItemText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Dialog from '@material-ui/core/Dialog';
-import PersonIcon from '@material-ui/icons/Person';
-import AddIcon from '@material-ui/icons/Add';
-import Typography from '@material-ui/core/Typography';
+
 import { blue } from '@material-ui/core/colors';
+import {
+  Button,
+  Typography,
+  Dialog,
+  DialogTitle,
+  DialogActions,
+  DialogContentText,
+  DialogContent,
+  TextField,
+} from '@material-ui/core';
 
 const emails = ['username@gmail.com', 'user02@gmail.com'];
 const useStyles = makeStyles({
@@ -35,41 +35,21 @@ export default function CreateEventDialog(props) {
   };
 
   return (
-    <Dialog
-      onClose={handleClose}
-      aria-labelledby="simple-dialog-title"
-      open={open}
-    >
-      <DialogTitle id="simple-dialog-title">Set backup account</DialogTitle>
-      <List>
-        {emails.map((email) => (
-          <ListItem
-            button
-            onClick={() => handleListItemClick(email)}
-            key={email}
-          >
-            <ListItemAvatar>
-              <Avatar className={classes.avatar}>
-                <PersonIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary={email} />
-          </ListItem>
-        ))}
+    <Dialog onClose={handleClose} open={open}>
+      <DialogTitle id="create-event">Create an event</DialogTitle>
+      <DialogContent>
+        <DialogContentText>Fill out the form</DialogContentText>
 
-        <ListItem
-          autoFocus
-          button
-          onClick={() => handleListItemClick('addAccount')}
-        >
-          <ListItemAvatar>
-            <Avatar>
-              <AddIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Add account" />
-        </ListItem>
-      </List>
+        <TextField variant="outlined" placeholder="title of event"></TextField>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color="secondary" variant="contained">
+          Add
+        </Button>
+        <Button onClick={handleClose} color="secondary" variant="contained">
+          never mind
+        </Button>
+      </DialogActions>
     </Dialog>
   );
 }
