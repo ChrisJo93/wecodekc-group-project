f6cf33dc0401d1046f25ed8f3a05bb2431a4fb79
Sarah Peters <sarahnpeters@gmail.com>
2021-01-12 13:47:48 -0600

    dialogs for add event on click

--- src/redux/reducers/event.reducer.js
+++ src/redux/reducers/event.reducer.js
@@ -2,6 +2,8 @@ const eventReducer = (state = [], action) => {
   switch (action.type) {
     case 'SET_EVENTS':
       return action.payload;
+    case 'SET_USER_EVENTS':
+      return action.payload;
     default:
       return state;
   }
