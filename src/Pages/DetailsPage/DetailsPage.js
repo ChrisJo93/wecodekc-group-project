60bb8f503453ad1e00b3dbf1c434bc4d811a1e70
Alex Campbell <alexbe.campbell@gmail.com>
2021-01-12 21:41:02 -0600

    data showing up on page, working on display and styling

--- src/Pages/DetailsPage/DetailsPage.js
+++ src/Pages/DetailsPage/DetailsPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, Typography, Paper, Button } from '@material-ui/core';
 
 import computer from './computer.jpg';
 import computer2 from './computer2.jpg';
@@ -31,20 +31,49 @@ class DetailsPage extends Component {
             <img src={computer2} alt="coding" />
           </Grid>
         </Grid>
-        <Grid container justify="space-evenly">
+        <div style={{ padding: 20 }}>
+          <Grid container spacing={3}>
+            <Grid xs={4} sm={4} md={4} lg={4}>
+              <Paper variant="outlined">{details.event_title}</Paper>
+              <Paper>Date: {details.event_start}</Paper>
+            </Grid>
+            <Grid container spacing={3}>
+              <Grid xs={4} sm={4} md={4} lg={4}>
+                <Paper>{details.event_address}</Paper>
+              </Grid>
+            </Grid>
+            <div style={{ padding: 20 }}>
+              <Grid container spacing={3}>
+                <Grid xs={4} sm={4} md={4} lg={4}>
+                  <Paper>Description: {details.event_description}</Paper>
+                </Grid>
+              </Grid>
+            </div>
+            <Grid container spacing={3}>
+              <Grid item xs={6}>
+                <Button variant="contained" color="secondary">
+                  Click to attend Event
+                </Button>
+              </Grid>
+              <Grid item xs={6}>
+                <Button variant="contained" color="primary">
+                  Back to Events
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </div>
+        {/* <Grid container justify="space-evenly">
           <Grid item>
             <Typography variant="h3" component="h2" gutterBottom>
-              Details!{JSON.stringify(this.props.store.eventDetailReducer[0])}
-              {details.event_description}
+              <p>{details.event_title}</p>
             </Typography>
           </Grid>
           <Grid item></Grid>
         </Grid>
         <Grid container spacing={3}>
-          <Typography variant="h3" component="h2" gutterBottom>
-            <div>{details.event_title}</div>
-          </Typography>
-        </Grid>
+          <Typography variant="h3" component="h2" gutterBottom></Typography>
+        </Grid> */}
       </div>
     );
   }
--- src/redux/reducers/event.details.reducer.js
+++ src/redux/reducers/event.details.reducer.js
@@ -1,7 +1,7 @@
 const eventDetailReducer = (state = {}, action) => {
   switch (action.type) {
     case 'SET_EVENT_DETAILS':
-      return action.payload;
+      return action.payload[0];
     default:
       return state;
   }
